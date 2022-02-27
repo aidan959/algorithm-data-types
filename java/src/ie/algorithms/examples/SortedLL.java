7aab7e9cc0ed88175003fd9f7408db6c32c18773
Aidan Dowling <6438565+aidan959@users.noreply.github.com>
2022-02-27 23:45:17 +0000

    Fixed and finished SortedLL code - may add a 'DataType' implementation to provide more consistent operation between datatypes

--- java/src/ie/algorithms/examples/SortedLL.java
+++ java/src/ie/algorithms/examples/SortedLL.java
@@ -1,15 +1,21 @@
-package algorithms.examples;
+package ie.algorithms.examples;
 // Sorted linked list with a sentinel node
 // Skeleton code
 
-import java.util.Currency;
 
 public class SortedLL
 {
     // this function does not need a constructor
-    // we only need to define head when inserting 
+    // we only need to define head when inserting
     // first value
     public SortedLL(){
+    }
+    // this is used to identify the type of error presented by this class
+    public class EmptyLinkedListException extends RuntimeException{
+        // allows you to use runtimeexceptions constructor 
+        public EmptyLinkedListException(String errorMessage){
+            super(errorMessage);
+        }
     }
     Node head;
     class Node{
@@ -24,7 +30,6 @@ public class SortedLL
     // important method
     public void insertSort(int x)
     {
-
         // creates the new node to be inserted - new allocates
         // a space in memory for the Node to exist in
         Node t = new Node(x);
@@ -37,7 +42,6 @@ public class SortedLL
             // this moves the object head refers to
             // into t.next
             t.next = head;
-            
             // update the head to this new node
             // with the old head being stored now
             // in head.next
@@ -50,42 +54,69 @@ public class SortedLL
         Node curr = head;
         // check that the data is greater than the
         // the next item and that we are not at the end
-        // of the list - curr.next is null  
-        while(curr.data < x && curr.next != null){
+        // of the list - curr.next is null
+        while(curr.next != null && x > curr.next.data  ){
             // walk through the list
             // set curr to the next node
             curr = curr.next;
         }
-            // set ts next node to the node after current
-            t.next = curr.next;
-            // insert the new node into the list
-            curr.next = t;
-        
+        // insert the new node into the list
+        t.next = curr.next;
+        // set ts next node to the node after current
+        curr.next = t;
+
+
 
     }
 
     public boolean remove(int x) {
+        // if the head doesn't exist then return false
+        if(head == null){
+            return false;
+        }
         // our temporary list pointer
         Node curr = head;
-        Node prev = curr;
-        // checks that the data doesnt match the value
-        // and the list isnt at an end
-        while(curr.data != x && curr.next != null){
-            prev = curr;
+        // checks that the list isnt at the end AND 
+        // checks that the data doesnt match the value <- doesnt execute 
+        //                                                first block
+        while(curr.next != null && curr.next.data != x ){
             // walks through list
             curr = curr.next;
         }
         if(curr.data == x){
-            prev.next = curr.next;
+            // this checks if we are dealing with the first item in the linked list
+            if(curr.next == null){
+                head = null;
+            } else{
+                head = curr.next;
+            }
             return true;
         }
         return false;
     }
-    
+    public int popHead(){
+        // // you'd want this to throw some handlable
+        // // error - as if head is null it cannot be
+        // // popped - if I do that, every reference
+        // // to popHead will require a try catch block:(
+
+        // NEVER MIND THIS I LEARNED ABOUT RUNTIME EXCEPTIONS
+        // these just raise errors that will happen at run time
+        if(head == null){
+            throw new RuntimeException("");
+        }
+        // we save this temporary variable to return the value before pulling it
+        // from the list - the head = head.next cannot execute AFTER return
+        int data = head.data;
+        head = head.next;
+        return data;
+
+    }
+
     public boolean isEmpty() {
         return head == null;
     }
-    
+
     public void display()
     {
         Node t = head;
@@ -96,38 +127,29 @@ public class SortedLL
         }
         System.out.println("null\n");
     }
-    public static void main(String[] args)   
+    public static void main(String[] args)
     {
         SortedLL list = new SortedLL();
+        list.remove(10);
 
-        double x;
-        int i, r;
+        list.display();
         list.insertSort(10);
         list.insertSort(15);
         list.insertSort(20);
         list.remove(15);
-        list.display();
+        list.remove(10);
+        // inserts random data
+        list.insertSort((int)(100*Math.random()));
+        list.insertSort((int)(100*Math.random()));
+        list.insertSort((int)(100*Math.random()));
+        list.insertSort((int)(100*Math.random()));
+        list.insertSort((int)(100*Math.random()));
+        list.insertSort((int)(100*Math.random()));
+        list.insertSort((int)(100*Math.random()));
+        list.insertSort((int)(100*Math.random()));
+        list.insertSort((int)(100*Math.random()));
+        System.out.println(list.popHead());
 
-        /*
-           for(i = 1; i <= 5; ++i)  {
-           x =  (Math.random()*100.0);
-           r = (int) x; 
-           System.out.println("Inserting " + r);
-           list.insert(r);
-           list.display();            
-        }
-        */
-        
-        /*
-        while(!list.isEmpty())  {
-            System.out.println("\nInput a value to remove: ");
-            Scanner in = new Scanner(System.in);
-            r = in.nextInt();
-            if(list.remove(r)) {
-                System.out.println("\nSuccessfully removed: " + r);
-            list.display(); }
-            else System.out.println("\nValue not in list");                        
-        }
-        */
+        list.display();
     }
 }
